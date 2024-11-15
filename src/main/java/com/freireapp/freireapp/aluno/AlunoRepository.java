0072b74ff7bda081219b8cc462fdf6cd447cd554
Claudio Alves <claudio@pinaculodigital.com.br>
2024-11-15 07:26:40 -0300

    fix: Corrige o nome das tabelas nas consultas do Aluno

--- src/main/java/com/freireapp/freireapp/aluno/AlunoRepository.java
+++ src/main/java/com/freireapp/freireapp/aluno/AlunoRepository.java
@@ -18,9 +18,9 @@ public class AlunoRepository {
                 "    p.nome as professor, pf.especializacao AS cadeira,\n" +
                 "    COUNT(pr.id_presenca) AS faltas\n" +
                 "FROM \n" +
-                "    presenca pr\n" +
-                "INNER JOIN professor pf ON pr.fk_Professor_fk_Pessoa_id_pessoa = pf.fk_Pessoa_id_pessoa\n" +
-                "INNER JOIN pessoa p ON pf.fk_Pessoa_id_pessoa = p.id_pessoa\n" +
+                "    Presenca pr\n" +
+                "INNER JOIN Professor pf ON pr.fk_Professor_fk_Pessoa_id_pessoa = pf.fk_Pessoa_id_pessoa\n" +
+                "INNER JOIN Pessoa p ON pf.fk_Pessoa_id_pessoa = p.id_pessoa\n" +
                 "WHERE \n" +
                 "    pr.fk_Aluno_fk_Pessoa_id_pessoa = ? \n" +
                 "    AND pr.status = 0\n" +
@@ -31,10 +31,10 @@ public class AlunoRepository {
 
     public List<Map<String, Object>> getResultadoAvaliacao(Long id) {
         String sql = "select p2.especializacao as cadeira, a2.descricao, a2.nota, rp.`data`\n" +
-                "from pessoa p join aluno a on a.fk_Pessoa_id_pessoa = p.id_pessoa\n" +
-                "join realiza_aluno_professor_avaliacao rp on rp.fk_Aluno_fk_Pessoa_id_pessoa = p.id_pessoa\n" +
-                "join avaliacao a2 on a2.id_avaliacao = rp.fk_Avaliacao_id_avaliacao\n" +
-                "join professor p2 on p2.fk_Pessoa_id_pessoa = rp.fk_Professor_fk_Pessoa_id_pessoa\n" +
+                "from Pessoa p join Aluno a on a.fk_Pessoa_id_pessoa = p.id_pessoa\n" +
+                "join realiza_Aluno_Professor_Avaliacao rp on rp.fk_Aluno_fk_Pessoa_id_pessoa = p.id_pessoa\n" +
+                "join Avaliacao a2 on a2.id_avaliacao = rp.fk_Avaliacao_id_avaliacao\n" +
+                "join Professor p2 on p2.fk_Pessoa_id_pessoa = rp.fk_Professor_fk_Pessoa_id_pessoa\n" +
                 "where p.id_pessoa = ?";
         return jdbcTemplate.queryForList(sql,id);
     }
